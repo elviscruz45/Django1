39192f25ad0868932202ddb291598cf9ff4b0da2
Elvis <elviscruz45@gmail.com>
2022-08-18 08:56:48 -0500

    rev final:

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -8,11 +8,11 @@ urlpatterns=[
     # ex:/polls/
     path("", views.index,name="index"),
     # ex:/polls/5/
-    path("<int:question_id>/detail", views.detail,name="detail"),
+    path("<int:question_idd>/detail", views.detail,name="detail"),
     # ex:/polls/5/results
-    path("<int:question_id>/results/", views.results,name="results"),
+    path("<int:question_idd>/results/", views.results,name="results"),
     # ex:/polls/5/vote
-    path("<int:question_id>/vote", views.vote,name="vote"),
+    path("<int:question_idd>/vote", views.vote,name="vote"),
     
     
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -9,15 +10,16 @@ def index(request):
     return render(request,"polls/index.html",{
         "latest_question_list":latest_question_list})
 
-def detail(request, question_id):
-    question=get_object_or_404(Question,pk=question_id)
+def detail(request, question_idd):
+    question=get_object_or_404(Question,pk=question_idd)
     return render(request,"polls/detail.html",{
         "question":question
     })
 
-def results(request, question_id):
-    return HttpResponse(f"Estas viendo resultados de la pregunta numero {question_id}")
+def results(request, question_idd):
+    return HttpResponse(f"Estas viendo resultados de la pregunta numero {question_idd}")
 
 
-def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
+def vote(request, question_idd):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_idd}")
+
